4494e0752f6b3005c54a35a0426cc9e26344a983
allan <allan.nava@ies-italia.it>
2023-01-27 14:46:47 +0100

    added srt type for request and response

--- haivision/haivision.go
+++ haivision/haivision.go
@@ -19,6 +19,7 @@ type IHaivisionClient interface {
 	IsDebug() bool
 	// auth stuff
 	InitSession(username string, password string) (*BaseResponseInitSession, error)
+	GetSessionInfo() (*ResponseSessionInfo, error)
 	GetDeviceInfo() (*BaseResponseDeviceInfo, error)
 	// Streaming
 	GetRoutes(deviceId string) error
